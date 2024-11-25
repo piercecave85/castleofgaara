4d69743bc15d340dc11f8c694b3a39f947c1dc6b
Pierce Cave <piercecave@gmail.com>
2024-11-24 21:31:14 -0800

    Get Docker Working for Frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true, // Listen on all interfaces
+    port: 5173,
     proxy: {
       '/api': {
         target: 'http://10.0.0.66:8000', // Replace with your API URL
